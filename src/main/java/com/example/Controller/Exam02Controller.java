5cf2de5f73e17ac7d9963073ca948eee35255b0e
Kojiro0706 <buena_vista0706@i.softbank.jp>
2021-08-04 13:11:27 +0900

    初級課題3の完成
    exam03.htmlの作成
    Please enter the commit message for your changes. Lines starting
    
    Exam03Controllerの作成
    
    exam03-result.htmlの作成

---
+++ src/main/java/com/example/Controller/Exam02Controller.java
@@ -0,0 +1,35 @@
+package com.example.Controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class Exam02Controller {
+
+	@Controller
+	@RequestMapping("/ex02")
+	public class ExamController02 {
+		@RequestMapping("")
+		public String index() {
+			return "exam02";
+		}
+
+		@Autowired
+		private HttpSession session;
+
+		@RequestMapping("/add")
+		public String addition(Integer num1, Integer num2) {
+			session.setAttribute("num1", num1);
+			session.setAttribute("num2", num2);
+			session.setAttribute("answer", num1 + num2);
+			return "exam02-result";
+		}
+
+		@RequestMapping("/nextPage")
+		public String toNextPage() {
+			return "exam02-result2";
+		}
+	}
+}
